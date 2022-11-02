a4d182d97c686ac47967265c0359385c1450d5b2
Adil314 <adilmugxal@gmail.com>
2022-11-02 12:01:12 +0500

    ffgffffhfh

---
+++ Javascript/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    $('#pofile_ripple').ripples({
+        resolution:512,
+        dropradius:10
+    });
+
+    // var bars=document.querySelectorAll('.progress-bar');
+    // bars.forEach(function(bar) {
+        
+    // let percetage = bar.dataset.percent;
+    // let tooltip = bar.children[0];
+
+    //     tooltip.innerText=percetage+" %";
+    //     bar.style.width=percetage+"%";
+
+
+
+    // });
+
+});
